6f53b406d15237b75dd31bc0bf9847e583aaa312
wo <>
2023-12-14 02:07:10 +0800

    _

--- main/main.ts
+++
@@ -1,9 +0,0 @@
-import {kai} from "@/_4everland/kai";
-
-// s3.headObject({Bucket: "bytes", Key: "F3C70AD13D094EFB9CD4ACF37EDA0B55"}).then((d) => {
-//     console.log(d);
-// });
-
-// guan("bytes");
-// kai("bytes").then();
-// listBuckets();
